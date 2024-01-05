14b71701e0d4c3deda88f20ef7ad8c433b829a26
eugeneLuck <eugeneLuck@gmail.com>
2024-01-05 15:14:09 +0200

    Custover to Cash

--- ShopCashes/MainWindow.xaml.cs
+++ ShopCashes/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Security.Policy;
 using System.Text;
 using System.Windows;
@@ -17,25 +18,71 @@ namespace ShopCashes
 {
     public partial class MainWindow : Window
     {
-        public Shop shop = new(4);
+        public Shop shop;
+        Label newCustomer;
         int num;
 
         public MainWindow()
         {
             InitializeComponent();
-            cashes.ItemsSource = shop.Cashes;
-            hall.MouseLeftButtonDown += Hall_MouseLeftButtonDown;
 
-            shop.Cashes[1].Customers.Add(
-                new Customer(1, 10, 12, 5));
-            shop.Cashes[1].Customers.Add(
-                new Customer(2, 14, 12, 8));
+            shop = new()
+            {
+                CashCount = 4,
+                
+                MinNextCustomerInterval = 1,
+                MaxNextCustomerInterval = 5,
+
+                MinBuyingInterval = 5,
+                MaxBuyingInterval = 15,
+
+                MinPayingInterval = 1,
+                MaxPayingInterval = 3,
+            };
+            shop.Initialize();
+            shop.OnCustomerArrival += Shop_OnCustomerArrival;
+            shop.OnCustomerIntoHall += Shop_OnCustomerIntoHalll;
+            shop.OnCustomerToCash += Shop_OnCustomerToCash;
+
+            cashes.ItemsSource = shop.Cashes;
         }
 
-        private void Hall_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private async void Shop_OnCustomerArrival(object? sender, Customer e)
         {
-            shop.Cashes[0].Customers.Add(
-                new Customer(num++, num * 10, num * 10 + num, 5));
+            newCustomer = new Label()
+            {
+                Template = FindResource("CustomerTemplate") as ControlTemplate,
+                Width=20,
+                Height=20,
+                Tag = e
+            };
+            newCustomer.DataContext = e;
+            Grid.SetColumn(newCustomer, 0);
+            Grid.SetRow(newCustomer, 1);
+            grid.Children.Add(newCustomer);
+
+            Debug.WriteLine("???");
+        }
+
+        private async void Shop_OnCustomerIntoHalll(object? sender, Customer e)
+        {
+            grid.Children.Remove(newCustomer);
+            double x = Shop.Random.NextDouble() *
+                (hall.ActualWidth - newCustomer.ActualWidth);
+            double y = Shop.Random.NextDouble() *
+                (hall.ActualHeight - newCustomer.ActualHeight);
+            Canvas.SetLeft(newCustomer, x);
+            Canvas.SetTop(newCustomer, y);
+            hall.Children.Add(newCustomer);
+
+            Debug.WriteLine("!!!");
+        }
+
+        private void Shop_OnCustomerToCash(object? sender, Customer e)
+        {
+            Label customer = hall.Children.Cast<Label>().
+                Single(c => c.Tag as Customer == e);
+            hall.Children.Remove(customer);
         }
     }
 }
--- ShopCashesModel/Shop.cs
+++ ShopCashesModel/Shop.cs
@@ -1,20 +1,92 @@
 using System.Diagnostics;
+using System.Windows.Threading;
 
 namespace ShopCashesModel
 {
     public class Shop
     {
+        public static Random Random = new();
+        public static DispatcherTimer ModelTimer = new();
+        public static int ModelTime = 0;
+
+        public int CashCount { get; set; }
+
+        public int MinNextCustomerInterval { get; set; }
+        public int MaxNextCustomerInterval { get; set; }
+        int _nextCustomerInterval => Random.Next
+            (MinNextCustomerInterval, MaxNextCustomerInterval + 1); 
+
+        public int MinBuyingInterval { get; set; }
+        public int MaxBuyingInterval { get; set; }
+        int _nextBuyingInterval => Random.Next
+            (MinBuyingInterval, MaxBuyingInterval + 1);
+
+        public int MinPayingInterval { get; set; }
+        public int MaxPayingInterval { get; set; }
+        int _nextPayingInterval => Random.Next
+            (MinPayingInterval, MaxPayingInterval + 1);
+
         public List<Cash> Cashes { get; } = new();
 
-        public Shop(int cashCount)
+        int _nextCustomerTime;
+        int _nextCustomerId;
+
+        Customer _nextCustomer;
+        PriorityQueue<Customer, int> _customers = new();
+
+        bool _customerIntoHall;
+
+        public event EventHandler<Customer> OnCustomerArrival;
+        public event EventHandler<Customer> OnCustomerIntoHall;
+        public event EventHandler<Customer> OnCustomerToCash;
+
+        public void Initialize()
         {
-Debug.WriteLine("!!!\n Shop created \n!!!");
-            for(int i = 0; i < cashCount; i++)
+            for(int i = 0; i < CashCount; i++)
             {
                 Cashes.Add(new() { Id = i + 1 });
             }
-Debug.WriteLine($"!!!\n {Cashes[Cashes.Count-1].Id} \n!!!");
+
+            ModelTimer.Interval = TimeSpan.FromSeconds(0.5);
+            ModelTimer.Tick += ModelTimer_Tick;
+            ModelTimer.Start();
+        }
+
+        private void ModelTimer_Tick(object? sender, EventArgs e)
+        {
+            _nextCustomerTime = ModelTime + _nextCustomerInterval;
+            int nextCustomerBuyingTime = ModelTime + _nextBuyingInterval;
+
+            ModelTime++;
+            Debug.WriteLine($"Time: {ModelTime}");
+
+            if(_customerIntoHall)
+            {
+                _customers.Enqueue(_nextCustomer, nextCustomerBuyingTime);
+                OnCustomerIntoHall?.Invoke(this, _nextCustomer);
+                _customerIntoHall = false;
+            }
+
+            if(ModelTime == _nextCustomerTime)
+            {
+                int cashTime = ModelTime + _nextBuyingInterval;
+                _nextCustomer = new(_nextCustomerId++, ModelTime, 
+                    cashTime, _nextPayingInterval);
+
+                OnCustomerArrival?.Invoke(this, _nextCustomer);
+                _customerIntoHall = true;
+            }
+
+            while(_customers.Count > 0
+                && _customers.Peek().CashboxTime <= ModelTime)
+            {
+                var lessQueueCash = Cashes.MinBy(x => x.Customers.Count);
+                var customer = _customers.Dequeue();
+                lessQueueCash.Customers.Add(customer);
+
+                OnCustomerToCash?.Invoke(this, customer);
+            }
+
         }
     }
-
 }
